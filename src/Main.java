04a2942b93a3ac2a17e64f4d1011071203021803
Domenico Pazienza <d.pazienza1@studenti.uniba.it>
2021-03-25 12:45:18 +0100

    new version

--- src/Main.java
+++ src/Main.java
@@ -6,58 +6,38 @@ public class Main {
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		Biblioteca biblioteca = new Biblioteca();
 		Scanner input = new Scanner(System.in);
-		//Libro libro1 = new Libro();
-		//Libro libro2 = new Libro();
-		
-		
-		
+
+
+
 		System.out.println("quanti libri vuoi inserire?");
 		int n =input.nextInt();
-		
+
 		for(int i = 0; i < n; i++) {
-			
+
 			Libro libro = new Libro();
-			
+
 			System.out.println("inserisci il codice del libro");
 			libro.setCodice(i+1);
-			
+
 			System.out.println("inserisci il nome del libro");
 			libro.setNome("libro" + (i+1));
-			
+
 			biblioteca.setLibro(libro);
-			
-			
-			//biblioteca.getList();
-			
-			
-			
+
 		}
-		
-		/*
-		
-		libro1.setCodice(1);
-		libro1.setNome("libro1");
-		libro2.setCodice(2);
-		libro2.setNome("libro2");
-		
-		biblioteca.setLibro(libro1);
-		biblioteca.setLibro(libro2);
-		
-		biblioteca.getLibro(0);
-		biblioteca.getLibro(1);
-		*/
-		
+
+
 		biblioteca.getList();
-		
+
 		biblioteca.saveList();
 		biblioteca.chargeList();
-		
+
 		input.close();
-		
+
 		System.out.println("I libri salvati su file sono: \n");
 		biblioteca.getList();
-	
-	
+
+
 
 	}
 
